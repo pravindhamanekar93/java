2377fa9b48fc8ce558f35616c196a7db3d7cea97
pravindhamanekar93 <pravindhamanekar93@gmail.com>
2026-02-14 23:29:59 +0530

    opeining doc

---
+++ w3_school/Maths_pgm.java
@@ -0,0 +1,19 @@
+package w3_school;
+
+public class Maths_pgm {
+    public static void main(String[] args) {
+        // Math.max(5, 10);
+        System.out.println(Math.max(5, 10));
+        // Math.min(5, 10);
+        // Math.sqrt(64);
+        // Math.abs(-4.7);
+        // Math.pow(2, 8);
+        // Math.round(4.6); // 5
+        // Math.ceil(4.1); // 5.0
+        // Math.floor(4.9); // 4.0
+
+        // Math.random();
+        // int randomNum = (int)(Math.random() * 101); // 0 to 100
+
+    }
+}
