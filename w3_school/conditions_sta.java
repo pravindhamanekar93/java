4c96e42185a9ed14df65d090d6d1396502161e62
pravindhamanekar93 <pravindhamanekar93@gmail.com>
2026-02-14 23:33:22 +0530

    opeining doc

---
+++ w3_school/conditions_sta.java
@@ -0,0 +1,14 @@
+package w3_school;
+
+public class conditions_sta {
+    public static void main(String[] args) {
+
+        // boolean isRaining = true;
+
+        // if (isRaining) {
+        // System.out.println("Bring an umbrella!");
+        // }
+
+    }
+
+}
